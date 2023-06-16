adc82da0cf22aa9270eb1bcb5fc5a0be04bd50c9
Guilherme Lopes <mobills.guilherme.lo@toro.int.br>
2023-06-16 19:24:13 -0300

    feat: renomeando imagens de nave

--- movimentação.py
+++ movimentação.py
@@ -2,45 +2,45 @@ from tupy import *
 
 class Nave(Image):
     def __init__(self):
-        self.file = 'nv_V1.png'
+        self.file = 'spaceship.png'
         self.contador = 0
     
     def vira_direita(self):
-        if self.file == 'nv_V2D.png':
+        if self.file == 'spaceshipV2right.png':
             self.x += 5
             self.contador += 1
-        elif self.file == 'nv_V3D.png':
+        elif self.file == 'spaceshipV3right.png':
             self.x += 10
 
 
 
     def update(self):
         if keyboard.is_key_down('Right'):
-            if self.file == 'nv_V1.png':
-                self.file = 'nv_V2D.png'
-            if self.file == 'nv_V2D.png':
+            if self.file == 'spaceship.png':
+                self.file = 'spaceshipV2right.png'
+            if self.file == 'spaceshipV2right.png':
                 self.vira_direita()
             # elif self
         
         if keyboard.is_key_up('Right'):
-            self.file = 'nv_V1.png'
+            self.file = 'spaceship.png'
             self.contador = 0
 
 
 
         if keyboard.is_key_down('Left'):
-            if self.file == 'nv_V1.png':
-                self.file = 'nv_V2E.png'
+            if self.file == 'spaceship.png':
+                self.file = 'spaceshipV2left.png'
             if self.contador == 15:
-                self.file = 'nv_V3E.png'
-            if self.file == 'nv_V2E.png':
+                self.file = 'spaceshipV3left.png'
+            if self.file == 'spaceshipV2left.png':
                 self.x -= 7
                 self.contador +=1
-            elif self.file == 'nv_V3E.png':
+            elif self.file == 'spaceshipV3left.png':
                 self.x -= 12
         
         if keyboard.is_key_up('Left'):
-            self.file = 'nv_V1.png'
+            self.file = 'spaceship.png'
             self.contador = 0
 
 Navinha = Nave()
