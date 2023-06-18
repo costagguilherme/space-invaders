ee1a4301db5638274ceb459b55ab62c5aa8093ca
Lucas Perrone <lucaspramos21@gmail.com>
2023-06-17 23:56:26 -0300

    Alteração Movimentação e Disparos
    . Correção na movimentação do Jogador
    
    . Tiro possui uma imagem própria
    . Velocidade do disparo alterada
    . Jogo Limitado a três tiros por tela (se um jogador apertar a tecla espaço 4 vezes, só serão disparados 3 tiros e enquanto eles não sumirem da tela, o jogador não consegue atirar)

--- Shot.py
+++ Shot.py
@@ -2,17 +2,20 @@ from tupy import *
 
 class Shot(Image):
   def __init__(self):
-      self.file = 'enemieV1.png'
+      self.file = 'spaceship_shot.png'
       self.battlefield = None
 
   def update(self):
-    self.y-=5
+    self.y-=30
     for enemie in self.battlefield.enemies:
         if(self._collides_with(enemie)):
-            print(f'colidiu como o inimigo n° {enemie.id}')
+            print(f'colidiu com o inimigo n° {enemie.id}')
             enemie.destroy()
             self.destroy()
             self.battlefield.enemies.remove(enemie)
             self.battlefield.shots.remove(self)
+    if(self.y < 0):
+        self.destroy()
+        self.battlefield.shots.remove(self)
 
 
